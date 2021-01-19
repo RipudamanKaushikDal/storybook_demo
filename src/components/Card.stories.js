39706aba26d9e993a2b21fa77e08625a10afbf96
RipudamanKaushikDal <Ripudaman.kaushik@dal.ca>
2021-01-19 09:40:45 -0400

    Removed Warnings

--- src/components/Card.stories.js
+++ src/components/Card.stories.js
@@ -1,21 +1,24 @@
 import React from 'react';
 
-import Card  from './Card';
+import Card from './Card';
 
-export default {
+const cardStory = {
   title: 'Example/Card',
   component: Card,
   argTypes:
   {
-    Clicked:{action:'clicked'}
-},
+    Clicked: { action: 'clicked' }
+  },
 };
 
 const Template = (args) => <Card {...args} />;
 
 export const Shows = Template.bind({});
 Shows.args = {
-  Title:"DareDevil", 
-  ImageUri:"https://i.pinimg.com/originals/31/5d/d4/315dd46f45fe609566238e394ebf1726.jpg",
-  
+  Title: "DareDevil",
+  ImageUri: "https://i.pinimg.com/originals/31/5d/d4/315dd46f45fe609566238e394ebf1726.jpg",
+
 };
+
+
+export default cardStory
